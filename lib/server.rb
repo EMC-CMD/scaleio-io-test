6ec60f52256a68ae0747a75567cbd31302cd7bef
Xuebin He <xuebin.he@emc.com>
2016-06-06 10:10:38 -0400

    Add endpoint for acceptance test
    - Add ruby version in Gemfile
    
    [#120582171](https://www.pivotaltracker.com/story/show/120582171)

--- lib/server.rb
+++ lib/server.rb
@@ -36,8 +36,25 @@ def test_folder_path
   vcap_services["scaleiogo"][0]["volume_mounts"][0]["container_path"]
 end
 
+get "/data" do
+  statue_code = 200
+  body_content = ""
+  if File.exist?(File.join(test_folder_path, "test.txt"))
+    body_content = File.read(File.join(test_folder_path, "test.txt"))
+  else
+    status_code = 404
+  end
+
+  status status_code
+  body body_content
+end
+
+post "/data" do
+  request.body.rewind
+  File.write(File.join(test_folder_path, "test.txt"), request.body.read)
+end
+
 get "/" do
-  @filepath = test_folder_path
   if File.exist?(File.join(test_folder_path, "test.txt"))
     @filecontent = File.read(File.join(test_folder_path, "test.txt"))
   else
@@ -50,7 +67,6 @@ end
 
 post "/" do
   File.write(File.join(test_folder_path, "test.txt"), params[:filecontent])
-  @filepath = test_folder_path
   @instance = ENV["CF_INSTANCE_INDEX"]
   @filecontent = File.read(File.join(test_folder_path, "test.txt"))
   erb :index
